8098fcf07764716bb7b2b38d23c6291aa9d4060d
bsreeganeshsingh <87886530+bsreeganeshsingh@users.noreply.github.com>
2025-06-06 21:13:52 +0530

    Code changes made in the core concepts practical task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import Counter from './Counter';
+import SearchForm from './SearchForm';
+import GenreSelect from './GenreSelect';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [selectedGenre, setSelectedGenre] = React.useState('Action');
+  const genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi'];
+
+  const handleSearch = (query) => {
+    alert(`Search triggered for: ${query}`);
+  };
+
+  const handleGenreSelect = (genre) => {
+    alert(`Genre selected: ${genre}`);
+    setSelectedGenre(genre);
+  };
+
+  return React.createElement('div', { className: 'App' },
+    React.createElement('header', { className: 'App-header' },
+      React.createElement('h1', null, 'Welcome to the Core Concepts Demo App'),
+      React.createElement(Counter, { initialValue: 5 }),
+      React.createElement(SearchForm, { initialQuery: 'Tare Jameen Par', onSearch: handleSearch }),
+      React.createElement(GenreSelect, {
+        genres: genres,
+        selectedGenre: selectedGenre,
+        onSelect: handleGenreSelect
+      })
+    )
   );
 }
 
