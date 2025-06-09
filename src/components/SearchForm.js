4b031b1ebf95da379715fda7277a315c4ebcee27
bsreeganeshsingh <87886530+bsreeganeshsingh@users.noreply.github.com>
2025-06-09 15:47:38 +0530

    refactor: convert SearchForm.js to a functional component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,53 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
+import './Components.css';
 
-class SearchForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: props.initialQuery || ''
-    };
+function SearchForm({ initialQuery = '', onSearch }) {
+  const [query, setQuery] = useState(initialQuery);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleSearchClick  = this.handleSearchClick.bind(this);
-  }
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
 
-  handleChange(event) {
-    this.setState({ query: event.target.value });
-  }
-
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      this.triggerSearch();
+  const triggerSearch = () => {
+    if (onSearch) {
+      onSearch(query);
     }
-  }
+  };
 
-  handleSearchClick (event) {
-      this.triggerSearch();
-  }
-
-  triggerSearch() {
-    if (this.props.onSearch) {
-      this.props.onSearch(this.state.query);
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      triggerSearch();
     }
-  }
+  };
 
-  render() {
-    return React.createElement('div', { style: { textAlign: 'center', marginTop: '20px' } },
-      React.createElement('input', {
-        type: 'text',
-        value: this.state.query,
-        onChange: this.handleChange,
-        onKeyDown: this.handleKeyPress,
-        placeholder: 'Enter search query',
-        style: { padding: '8px', width: '200px', marginRight: '10px' }
-      }),
-      React.createElement('button', {
-        onClick: this.handleSearchClick,
-        style: { padding: '8px 12px' }
-      }, 'Search')
-    );
-  }
+  return (
+    <div className="search-form-container">
+      <input
+        type="text"
+        className="search-input"
+        value={query}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        placeholder="Enter search query"
+      />
+      <button className="button" onClick={triggerSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchForm;
